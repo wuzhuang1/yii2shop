5b5cc54a9e94076e723b197e76a0387155b63a32
yii2shop <1922648994@qq.com>
2017-07-18 22:46:20 +0800

    新加

---
+++ backend/views/brand/add.php
@@ -0,0 +1,14 @@
+<?php
+$form = \yii\bootstrap\ActiveForm::begin();
+//name	varchar(50)	名称
+echo $form->field($model,'name');
+//intro	text	简介
+echo $form->field($model,'intro')->textarea();
+//logo	varchar(255)	LOGO图片
+echo $form->field($model,'imgFile')->fileInput();
+//sort	int(11)	排序
+echo $form->field($model,'sort')->textInput(['type'=>'number']);
+//status	int(2)	状态(-1删除 0隐藏 1正常)
+echo $form->field($model,'status',['inline'=>true])->radioList(\backend\models\Brand::getStatusOptions());
+echo \yii\bootstrap\Html::submitButton('提交',['class'=>'btn btn-info']);
+\yii\bootstrap\ActiveForm::end();
