0c37527bc94516dcd247d369f439093b6f8f05b1
yii2shop <1922648994@qq.com>
2017-07-19 11:59:52 +0800

    新加列表

--- backend/views/brand/index.php
+++ backend/views/brand/index.php
@@ -1,9 +1,27 @@
+<?=\yii\bootstrap\Html::a('添加',['goods/add'],['class'=>'btn btn-sm btn-success'])?>
+<table class="table">
+    <tr>
+        <th>id</th>
+        <th>名称</th>
+        <th>简介</th>
+        <th>图片</th>
+        <th>状态</th>
+        <th>操作</th>
+    </tr>
+    <?php foreach ($model as $row):;?>
+    <tr>
+        <td><?=$row->id?></td>
+        <td><?=$row->name?></td>
+        <td><?=$row->intro?></td>
+        <td><?=$row->id?></td>
+        <td><?=$row->status?'显示':'隐藏'?></td>
+        <td>
+            <?=\yii\bootstrap\Html::a('修改',['brand/edit','id'=>$row->id],['class'=>'btn btn-sm btn-success'])?>
+            <?=\yii\bootstrap\Html::a('删除',['brand/del','id'=>$row->id],['class'=>'btn btn-sm btn-success'])?>
+        </td>
+    </tr>
+    <?php endforeach;?>
+</table>
 <?php
-/* @var $this yii\web\View */
-?>
-<h1>brand/index</h1>
-
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+//分页工具条
+echo \yii\widgets\LinkPager::widget(['pagination'=>$pager,'nextPageLabel'=>'下一页','prevPageLabel'=>'上一页','firstPageLabel'=>'首页']);
